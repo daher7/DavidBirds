e08f1711b510168594abd473cb9f5a9a9afa5c00
daher7 <daher7@gmail.com>
2018-11-09 11:33:23 +0100

    Avanzando

---
+++ Assets/_MyGameAssets/Scripts/PajaroScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PajaroScript : MonoBehaviour {
+
+	
+
+	void Update () {
+        // Recogemos la pulsacion del dedo
+        Touch t = Input.GetTouch(0);
+        // Identificador del puntero
+        int id = t.fingerId;
+        // Posicion del puntero en la pantalla
+        Vector2 position = t.position;
+	}
+}
