83523ed46071be49ed19bb715f97820e2e6ab749
daher7 <daher7@gmail.com>
2018-11-12 10:24:16 +0100

    Score

---
+++ Assets/_MyGameAssets/Scripts/CajaScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaScript : MonoBehaviour {
+
+    [SerializeField] GameObject pfPuntuacion;
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            MostrarPuntuacion();
+            //Destroy(this.gameObject);
+        }
+    }
+
+    private void MostrarPuntuacion() {
+
+        GameObject puntuacion = Instantiate(pfPuntuacion, transform.position, transform.rotation);
+    }
+}
